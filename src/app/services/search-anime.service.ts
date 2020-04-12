0df5b6e6e3eab9d92ddacc103e973276ed9be6ca
Jean Lucas <jean@4ray.co>
2020-04-12 19:17:56 -0400

    Typo

--- src/app/services/search-anime.service.ts
+++ src/app/services/search-anime.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Anime } from '../models/Anime'
+import { Anime } from '../models/anime'
 import { Observable } from 'rxjs';
 
 @Injectable({
