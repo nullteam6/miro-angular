c05307a64f2b51956181376c2a4d14ddbd6fa51f
Jean Lucas <jean@4ray.co>
2020-04-15 18:40:56 -0400

    Update style for profile-display component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,26 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
+import { ProfileDisplayComponent } from '../profile-display/profile-display.component';
+
 import { UserService } from '../services/user.service';
 
 import { Profile } from '../models/profile';
-
-
-import { JsonParse } from '../pipes/json-parse';
-import { ProfileDisplayComponent } from '../profile-display/profile-display.component';
-
-
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.sass'],
-  providers: [JsonParse]
 })
 export class ProfileComponent implements OnInit {
   public profile = new Profile();
-  public animeListArr = [];
   @ViewChild(ProfileDisplayComponent) profileDisplay: ProfileDisplayComponent;
-  
+
   constructor(
     private userService: UserService,
   ) {
@@ -33,5 +27,4 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() { }
-
 }
